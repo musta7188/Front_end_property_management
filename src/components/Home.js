2bf4ed4b09a17aa9394642c0cf9e208e98ee003d
musta7188 <musta7188@hotmail.it>
2020-04-11 20:59:58 +0100

    log in and show only when user is not logged-in

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,14 +14,14 @@ const Home = (props) => {
     .catch(error => console.log(error))
   }
 
-
+  
   return(
     <div>
-      <Link to='/login'>Log In</Link>
-      <br></br>
-      <Link to='/signup'>Sign Up</Link>
-      <br></br>
-      { props.loggedInStatus ? <Link to='/logout' onClick={handleClick} >Log Out</Link> : null
+      { props.loggedInStatus ?
+      <Link to='/logout' onClick={handleClick} >Log Out</Link> :  
+     <div> <Link to='/login'>Log In</Link> 
+     <br></br> 
+     <Link to='/signup'>Sign Up</Link></div>
     }
     </div>
   );
