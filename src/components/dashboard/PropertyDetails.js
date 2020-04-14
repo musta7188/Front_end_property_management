e4ecb0226c33987d281ca6927512c1aab87c916b
alexandre jamarco <alexandre.j@gmail.com>
2020-04-14 20:09:57 +0100

    passed all information to property details

--- src/components/dashboard/PropertyDetails.js
+++ src/components/dashboard/PropertyDetails.js
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react'
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 
 export default function PropertyDetails({property, showAllProperties}) {
 
@@ -12,11 +14,21 @@ export default function PropertyDetails({property, showAllProperties}) {
     }, [])
 
     // render the details after is loaded
+    const renderDetails = () =>{
+        const tenant = details.tenants[0];
+        return(
+            <React.Fragment>
+                <h2>{details.property.address}</h2>
+                <p>{tenant.first_name}</p>
+            </React.Fragment>
+        )
+    }
 
+    //main return
     return (
-        <div>
-            <h1>{details !== 0 ? details.property.address : null}</h1>
-            <button onClick={showAllProperties}>Go back</button>
-        </div>
+        <Container maxWidth="xl">
+            <div>{details !== 0 ? renderDetails() : null}</div>
+            <Button onClick={showAllProperties} variant="contained" color="primary">Go back</Button>
+        </Container>
     )
 }
